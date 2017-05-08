8dc18080d9bba8d448ff6b538ede20e6fa63e29a
Nzzzz <467273751@qq.com>
2017-05-08 16:13:53 +0800

    master
    Signed-off-by: Nzzzz <467273751@qq.com>

--- bootstrap/main/angularjs_1.js
+++ bootstrap/main/angularjs_1.js
@@ -10,4 +10,11 @@ angular.module("myApp",[])
 	$scope.del = function(index) {
 		$scope.app.containers.splice(index,1);
 	}
+	$scope.arrTest = [];
+	var quotas = [{cpu:"1"},{cpu:"2"}];
+	for(var i=0;i<5;i++){
+		$scope.arrTest[i] = angular.copy(quotas);
+	}
+	$scope.arrTest[1][0].cpu = "x";
+	console.log($scope.arrTest);
 })
